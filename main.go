4b19a0f6043dbccfc1711beb5bec2784be49b920
shellyyg <chih-ching.yang@whu.edu>
2022-11-15 10:14:25 +0100

    Replace context with logic router at Decider

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,99 +9,46 @@ import (
 var key1 = "KEY1"
 var key2 = "KEY2"
 
-type Service struct {
-	ClientName string
-	IParser
-	IMapper
+type Decider struct {
+	parser map[string]IParser
 }
 type IParser interface {
 	InnerParse(in string) (out string) // TODO: change to Parser Signature
 }
-type IMapper interface {
-	InnerMap(in string) (out string)
+
+func (d *Decider) AddCustomer(name string, parser IParser) {
+	// Initiate map to avoid nil panic
+	if d.parser == nil {
+		d.parser = make(map[string]IParser)
+	}
+	d.parser[name] = parser
 }
 
-func Decider(next http.Handler) http.Handler {
+func (d *Decider) Decide(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("Start Deciding...")
-		var handlerConfig = map[string]Service{
-			"kun": {
-				ClientName: "kun",
-				IParser:    &KunParserConcreteType{},
-				IMapper:    &KunMapperConcreteType{},
-			},
-		}
-
-		var realTimeClient = "kun"
-
-		var Service = handlerConfig[realTimeClient]
-		ctxWithService := context.WithValue(r.Context(), "Service", Service)
-		rWithService := r.WithContext(ctxWithService)
-		next.ServeHTTP(w, rWithService)
+		var realTimeClient = "DHL"
+		parser := d.parser[realTimeClient]
+		result := parser.InnerParse("Hi") // change to Service.Serve
+		fmt.Println("result: ", result)
+		next.ServeHTTP(w, r)
 		log.Print("End Deciding.")
-	})
-}
-func Parse(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("@Kun Start Parsing...")
-		ctx := r.Context()
-		svc, ok := ctx.Value("Service").(Service)
-		if !ok {
-			fmt.Println("oops parsing")
-		}
-		var in = "一"
-		var out = svc.IParser.InnerParse(in)
-		ctxWithNewParam1 := context.WithValue(r.Context(), key1, out)
-		rWithP1 := r.WithContext(ctxWithNewParam1)
-		next.ServeHTTP(w, rWithP1)
-		log.Print("@Kun End Parsing.")
-	})
-}
-func Map(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("@Kun Start Mapping...")
-		ctx := r.Context()
-		svc, ok := ctx.Value("Service").(Service)
-		if !ok {
-			fmt.Println("oops mapping")
-		}
-		var in = "一"
-		var out = svc.IMapper.InnerMap(in)
-		ctxWithNewParam2 := context.WithValue(r.Context(), key2, out)
-		rWithP2 := r.WithContext(ctxWithNewParam2)
-		next.ServeHTTP(w, rWithP2)
-		log.Print("@Kun End Mapping.")
 	})
 }
 
 type KunParserConcreteType struct{}
 
-func (k *KunParserConcreteType) InnerParse(in string) (out string) {
+// Real Parser logic
+func (k KunParserConcreteType) InnerParse(in string) (out string) {
 	var returnVal = in + "_KUN_parsed"
 	return returnVal
 }
 
-type KunMapperConcreteType struct{}
+type DHLParserConcreteType struct{}
 
-func (k *KunMapperConcreteType) InnerMap(in string) (out string) {
-	var returnVal = in + "_KUN_mapped"
+// Real Parser logic
+func (k DHLParserConcreteType) InnerParse(in string) (out string) {
+	var returnVal = in + "_DHL_parsed"
 	return returnVal
-}
-
-func middlewareThree(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("Executing m3")
-		p1 := r.Context().Value(key1)
-		p2 := r.Context().Value(key2)
-
-		fmt.Println("p1: ", p1, "p2: ", p2)
-
-		if r.URL.Path == "/foo" {
-			return
-		}
-		next.ServeHTTP(w, r)
-		log.Print("Executing m3 again")
-	})
 }
 
 func final(w http.ResponseWriter, r *http.Request) {
@@ -112,10 +58,16 @@ func final(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
+	var newKunParser = KunParserConcreteType{}
+	var newDHLParser = DHLParserConcreteType{}
+
+	var d Decider
+	d.AddCustomer("kun", newKunParser)
+	d.AddCustomer("DHL", newDHLParser)
 
 	finalHandler := http.HandlerFunc(final)
 
-	mux.Handle("/", Decider(Parse(Map(middlewareThree(finalHandler)))))
+	mux.Handle("/", d.Decide((finalHandler)))
 
 	log.Print("Listening on :3000...")
 	err := http.ListenAndServe(":3000", mux)
