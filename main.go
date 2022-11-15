38b8250d43fc98fef58d9366265f9c575d00f925
shellyyg <chih-ching.yang@whu.edu>
2022-11-15 11:58:33 +0100

    Wrap Parser in Service

--- main.go
+++ main.go
@@ -10,26 +10,43 @@ var key1 = "KEY1"
 var key2 = "KEY2"
 
 type Decider struct {
-	parser map[string]IParser
-}
-type IParser interface {
-	InnerParse(in string) (out string) // TODO: change to Parser Signature
+	service map[string]Service
 }
 
-func (d *Decider) AddCustomer(name string, parser IParser) {
+type Service struct {
+	ClientName string
+	Parser     IParser
+}
+
+type IParser interface {
+	Parse(in string) (out string) // TODO: change to Parser Signature
+}
+
+func (s *Service) Serve(in string) {
+	result := s.Parser.Parse(in)
+	fmt.Println("parsed result: ", result)
+}
+
+func (d *Decider) AddCustomer(name string, service Service) {
 	// Initiate map to avoid nil panic
-	if d.parser == nil {
-		d.parser = make(map[string]IParser)
+	if d.service == nil {
+		d.service = make(map[string]Service)
 	}
-	d.parser[name] = parser
+	d.service[name] = service
 }
 
 func (d *Decider) Decide(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var realTimeClient = "DHL"
-		parser := d.parser[realTimeClient]
-		result := parser.InnerParse("Hi") // change to Service.Serve
-		fmt.Println("result: ", result)
+		log.Print("Start Deciding.")
+		var realTimeClient = "kun"
+		var ToFull = make(map[string]string)
+		ToFull["clientName"] = realTimeClient
+		for _, toAddress := range ToFull {
+			if service, ok := d.service[toAddress]; ok {
+				service.Serve("Hi")
+			}
+		}
+
 		next.ServeHTTP(w, r)
 		log.Print("End Deciding.")
 	})
@@ -38,7 +55,7 @@ func (d *Decider) Decide(next http.Handler) http.Handler {
 type KunParserConcreteType struct{}
 
 // Real Parser logic
-func (k KunParserConcreteType) InnerParse(in string) (out string) {
+func (k KunParserConcreteType) Parse(in string) (out string) {
 	var returnVal = in + "_KUN_parsed"
 	return returnVal
 }
@@ -46,7 +63,7 @@ func (k KunParserConcreteType) InnerParse(in string) (out string) {
 type DHLParserConcreteType struct{}
 
 // Real Parser logic
-func (k DHLParserConcreteType) InnerParse(in string) (out string) {
+func (k DHLParserConcreteType) Parse(in string) (out string) {
 	var returnVal = in + "_DHL_parsed"
 	return returnVal
 }
@@ -58,12 +75,22 @@ func final(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	mux := http.NewServeMux()
-	var newKunParser = KunParserConcreteType{}
-	var newDHLParser = DHLParserConcreteType{}
+	var handlerConfig = map[string]Service{
+		"kun": {
+			ClientName: "kun",
+			Parser:     &KunParserConcreteType{},
+		},
+		"DHL": {
+			ClientName: "DHL",
+			Parser:     &DHLParserConcreteType{},
+		},
+	}
+	var newKunService = handlerConfig["kun"]
+	var newDHLService = handlerConfig["DHL"] // DHLParserConcreteType{}
 
 	var d Decider
-	d.AddCustomer("kun", newKunParser)
-	d.AddCustomer("DHL", newDHLParser)
+	d.AddCustomer("kun", newKunService)
+	d.AddCustomer("DHL", newDHLService)
 
 	finalHandler := http.HandlerFunc(final)
 
